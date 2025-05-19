5d60d2e287cb7cbad1d2847353fcb947d7299221
Greg42 <gregory.godin@gmail.com>
2025-05-19 22:02:21 +0200

    fix: utf8 mismatch

--- cookbook/samples/expense_report.py
+++ cookbook/samples/expense_report.py
@@ -14,7 +14,7 @@ from pipelex.run import run_pipe_code
 
 
 def read_text_file(file_path: str) -> str:
-    with open(file_path, "r") as f:
+    with open(file_path, "r", encoding="utf-8") as f:
         return f.read()
 
 
